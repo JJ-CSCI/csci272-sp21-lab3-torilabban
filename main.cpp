1b2a91cd98ffe7a7f4890a3d9fe46744b4dc0a3c
Victoria Labban <torilabban@gmail.com>
2021-03-03 20:06:04 +0000

    Curly Brackets & Main

--- main.cpp
+++ main.cpp
@@ -13,75 +13,64 @@ class Quaternion {
   double coeffe2;
   double coeffe3;
   public:
-  Quaternion ( double a, double bi , double cj, double dk){
+  Quaternion  ( double a, double bi , double cj, double dk){
     coeffe= a;
     coeffe1=bi;
     coeffe2=cj;
     coeffe3=dk;
   }
 
-};
+}
 
-int main(){
-
-Quaternion operator+ (const Quaternion& addie) const{
+Quaternion  operator+ (const Quaternion& addie) const{
   Quaternion addie{0,0,0,0};
-  addie.coeffe= this ->coeffe+addie.coeffe;
-  addie.coeffe1= this -> coeffe1+addie.coeffe1;
-  addie.coeffe2= this -> coeffe2 + addie.coeffe2;
-  addie.coeffe3= this-> coeffe3 + addie.coeffe3;
+  addie.coeffe= this ->q.coeffe+addie.coeffe;
+  addie.coeffe1= this ->q.coeffe1+addie.coeffe1;
+  addie.coeffe2= this ->q.coeffe2 + addie.coeffe2;
+  addie.coeffe3= this->q.coeffe3 + addie.coeffe3;
   return addie;
-  }
+}
 
-  Quaternion operator- (const Quaternion& min) const{
+Quaternion operator- (const Quaternion& min) const{
   Quaternion min{0,0,0,0};
   min.coeffe= this ->coeffe-min.coeffe;
   min.coeffe1= this -> coeffe1-min.coeffe1;
   min.coeffe2= this -> coeffe2 - min.coeffe2;
   min.coeffe3= this-> coeffe3 - min.coeffe3;
   return min;
-  }
+}
 
-  Quaternion operator* (const Quaternion& mul) const {
+Quaternion operator* (const Quaternion& mul) const {
   mul.coeffe= this ->coeffe*mul.coeffe;
   mul.coeffe1= this -> coeffe1*mul.coeffe1;
   mul.coeffe2= this -> coeffe2*mul.coeffe2;
   mul.coeffe3= this-> coeffe3*mul.addie.coeffe3;
-  return min; 
+  return min;
+} 
 
-  bool Quaternion operator== (const Quaternion& comp) const{
-    Quaternion comp;
-    if (this->coeffe==comp.coeffe){
+bool Quaternion operator== (const Quaternion& comp) const{
+  Quaternion comp;
+  if (this->coeffe==comp.coeffe){
       return true;}
+      else{
+        return false;}
+
+  if (this-> coeffe1==comp.coeffe1) {
+      return true;}
+      else { 
+        return false;}
+
+  if (this->coeffe2==comp.coeffe2) {
+    return true;}
     else{
       return false;}
 
-    if (this-> coeffe1==comp.coeffe1) {
-      return true;
-    }
-    else { 
+  if(this -> coeffe3==comp.coeffe3) {
+    return true;}
+    else{
       return false;}
+}
 
-    if (this->coeffe2==comp.coeffe2) {
-      return true;
-    }
-    else{
-      return false;
-    }
-
-    if(this -> coeffe3==comp.coeffe3) {
-      return true;
-    }
-    else{
-      return false;
-    }
-
-  
-    
-   
-
-  }
-};
 
 
 //------------------------------
