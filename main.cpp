efd44d5d72680c34e17f0971f24b27355ad2c766
Victoria Labban <torilabban@gmail.com>
2021-03-03 19:04:50 +0000

    I have errors but a website gave me the idea of what was needed of me.

--- main.cpp
+++ main.cpp
@@ -4,40 +4,84 @@
 #include "catch.hpp"
 //------------------------------
 // Put the assignment code here
+//https://www.tutorialspoint.com/cplusplus/cpp_overloading.htm
+#include <iostream>
 class Quaternion {
   private:
-  double a;
-  double bi;
-  double cj;
-  double dk;
+  double coeffe;
+  double coeffe1;
+  double coeffe2;
+  double coeffe3;
   public:
-  Quaternion ( double a, double bi , double cj, double dk);
-  bool operator==(const Quaternion &rhs)const{
-    return Quaternion == rhs.Quaternion;
-    } 
-  double Quaternion operator+ (const double &obj);
+  Quaternion ( double a, double bi , double cj, double dk){
+    coeffe= a;
+    coeffe1=bi;
+    coeffe2=cj;
+    coeffe3=dk;
+  }
+
 };
 
-void Quaternion:: Quaternion.q ( double , double, double , double);
-void Quaternion:: Quaternion.p ( double , double, double , double);
-void Quaternion:: Quaternion.r ( double , double, double , double);
-
 int main(){
-Quaternion q,p,r;
-void Quaternion::bool operator==(const Quaternion &rhs)const{
-  if ( Quaternion.q == Quaternion.p) {
-    cout<< " True " << endl;
-  }
-  else{
-    cout << " False" << endl;
+
+Quaternion operator+ (const Quaternion& addie) const{
+  Quaternion addie{0,0,0,0};
+  addie.coeffe= this ->coeffe+addie.coeffe;
+  addie.coeffe1= this -> coeffe1+addie.coeffe1;
+  addie.coeffe2= this -> coeffe2 + addie.coeffe2;
+  addie.coeffe3= this-> coeffe3 + addie.coeffe3;
+  return addie;
   }
 
-double Quaternion operator+ (const double &obj){
-  if (q)
-}
+  Quaternion operator- (const Quaternion& min) const{
+  Quaternion min{0,0,0,0};
+  min.coeffe= this ->coeffe-min.coeffe;
+  min.coeffe1= this -> coeffe1-min.coeffe1;
+  min.coeffe2= this -> coeffe2 - min.coeffe2;
+  min.coeffe3= this-> coeffe3 - min.coeffe3;
+  return min;
+  }
 
-}
-}
+  Quaternion operator* (const Quaternion& mul) const {
+  mul.coeffe= this ->coeffe*mul.coeffe;
+  mul.coeffe1= this -> coeffe1*mul.coeffe1;
+  mul.coeffe2= this -> coeffe2*mul.coeffe2;
+  mul.coeffe3= this-> coeffe3*mul.addie.coeffe3;
+  return min; 
+
+  bool Quaternion operator== (const Quaternion& comp) const{
+    Quaternion comp;
+    if (this->coeffe==comp.coeffe){
+      return true;}
+    else{
+      return false;}
+
+    if (this-> coeffe1==comp.coeffe1) {
+      return true;
+    }
+    else { 
+      return false;}
+
+    if (this->coeffe2==comp.coeffe2) {
+      return true;
+    }
+    else{
+      return false;
+    }
+
+    if(this -> coeffe3==comp.coeffe3) {
+      return true;
+    }
+    else{
+      return false;
+    }
+
+  
+    
+   
+
+  }
+};
 
 
 //------------------------------
